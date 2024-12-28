5f62290bc069ce0f6cd7b2dd44edd760b9b9113a
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2024-12-28 15:36:19 -0500

    reorder list

--- daemon/events/events.go
+++ daemon/events/events.go
@@ -2,14 +2,15 @@ package events
 
 const (
 	NewBlock                  string = `new_block`
-	TransactionAddedInMempool string = `transaction_added_in_mempool`
-	TransactionExecuted       string = `transaction_executed`
+	BlockOrdered              string = `block_ordered`
+	BlockOrphaned             string = `block_orphaned`
 	StableHeightChanged       string = `stable_height_changed`
 	StableTopoHeightChanged   string = `stable_topo_height_changed`
 	TransactionOrphaned       string = `transaction_orphaned`
+	TransactionAddedInMempool string = `transaction_added_in_mempool`
+	TransactionExecuted       string = `transaction_executed`
+	InvokeContract            string = `invoke_contract`
 	DeployContract            string = `deploy_contract`
-	BlockOrdered              string = `block_ordered`
-	BlockOrphaned             string = `block_orphaned`
 	NewAsset                  string = `new_asset`
 	PeerConnected             string = `peer_connected`
 	PeerDisconnected          string = `peer_disconnect`

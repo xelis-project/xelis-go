b544487cbec5ef7f878009453cc9710ca98a1126
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-01-28 15:38:48 -0500

    add request func encapsulation

--- daemon/rpc.go
+++ daemon/rpc.go
@@ -1,6 +1,8 @@
 package daemon
 
 import (
+	"net/http"
+
 	"github.com/xelis-project/xelis-go-sdk/daemon/methods"
 	"github.com/xelis-project/xelis-go-sdk/rpc"
 )
@@ -22,343 +24,351 @@ func NewRPC(url string) (*RPC, error) {
 	return daemon, nil
 }
 
+func (d *RPC) BatchRequest(requests []rpc.RPCRequest, result map[int64]interface{}) (res *http.Response, errs []error) {
+	return d.http.BatchRequest(requests, result)
+}
+
+func (d *RPC) Request(method string, params interface{}, result interface{}) (res *http.Response, err error) {
+	return d.http.Request(method, params, result)
+}
+
 func (d *RPC) GetVersion() (version string, err error) {
-	_, err = d.http.Request(methods.GetVersion, nil, &version)
+	_, err = d.Request(methods.GetVersion, nil, &version)
 	return
 }
 
 func (d *RPC) GetInfo() (result GetInfoResult, err error) {
-	_, err = d.http.Request(methods.GetInfo, nil, &result)
+	_, err = d.Request(methods.GetInfo, nil, &result)
 	return
 }
 
 func (d *RPC) GetHeight() (height uint64, err error) {
-	_, err = d.http.Request(methods.GetHeight, nil, &height)
+	_, err = d.Request(methods.GetHeight, nil, &height)
 	return
 }
 
 func (d *RPC) GetTopoheight() (topoheight uint64, err error) {
-	_, err = d.http.Request(methods.GetTopoheight, nil, &topoheight)
+	_, err = d.Request(methods.GetTopoheight, nil, &topoheight)
 	return
 }
 
 func (d *RPC) GetStableHeight() (stableheight uint64, err error) {
-	_, err = d.http.Request(methods.GetStableHeight, nil, &stableheight)
+	_, err = d.Request(methods.GetStableHeight, nil, &stableheight)
 	return
 }
 
 func (d *RPC) GetStableTopoheight() (topoheight uint64, err error) {
-	_, err = d.http.Request(methods.GetStableTopoheight, nil, &topoheight)
+	_, err = d.Request(methods.GetStableTopoheight, nil, &topoheight)
 	return
 }
 
 func (d *RPC) GetStableBalance(params GetBalanceParams) (result GetStableBalanceResult, err error) {
-	_, err = d.http.Request(methods.GetStableBalance, params, &result)
+	_, err = d.Request(methods.GetStableBalance, params, &result)
 	return
 }
 
 func (d *RPC) GetBlockTemplate(addr string) (result GetBlockTemplateResult, err error) {
 	params := map[string]string{"address": addr}
-	_, err = d.http.Request(methods.GetBlockTemplate, params, &result)
+	_, err = d.Request(methods.GetBlockTemplate, params, &result)
 	return
 }
 
 func (d *RPC) GetBlockAtTopoheight(params GetBlockAtTopoheightParams) (block Block, err error) {
-	_, err = d.http.Request(methods.GetBlockAtTopoheight, params, &block)
+	_, err = d.Request(methods.GetBlockAtTopoheight, params, &block)
 	return
 }
 
 func (d *RPC) GetBlocksAtHeight(params GetBlocksAtHeightParams) (blocks []Block, err error) {
-	_, err = d.http.Request(methods.GetBlocksAtHeight, params, &blocks)
+	_, err = d.Request(methods.GetBlocksAtHeight, params, &blocks)
 	return
 }
 
 func (d *RPC) GetBlockByHash(params GetBlockByHashParams) (block Block, err error) {
-	_, err = d.http.Request(methods.GetBlockByHash, params, &block)
+	_, err = d.Request(methods.GetBlockByHash, params, &block)
 	return
 }
 
 func (d *RPC) GetTopBlock(params GetTopBlockParams) (block Block, err error) {
-	_, err = d.http.Request(methods.GetTopBlock, params, &block)
+	_, err = d.Request(methods.GetTopBlock, params, &block)
 	return
 }
 
 func (d *RPC) GetNonce(addr string) (nonce GetNonceResult, err error) {
 	params := map[string]string{"address": addr}
-	_, err = d.http.Request(methods.GetNonce, params, &nonce)
+	_, err = d.Request(methods.GetNonce, params, &nonce)
 	return
 }
 
 func (d *RPC) HasNonce(addr string) (hasNonce bool, err error) {
 	params := map[string]string{"address": addr}
 	var result map[string]bool
-	_, err = d.http.Request(methods.HasNonce, params, &result)
+	_, err = d.Request(methods.HasNonce, params, &result)
 	hasNonce = result["exist"]
 	return
 }
 
 func (d *RPC) GetNonceAtTopoheight(params GetNonceAtTopoheightParams) (nonce VersionedNonce, err error) {
-	_, err = d.http.Request(methods.GetNonceAtTopoheight, params, &nonce)
+	_, err = d.Request(methods.GetNonceAtTopoheight, params, &nonce)
 	return
 }
 
 func (d *RPC) GetBalance(params GetBalanceParams) (balance GetBalanceResult, err error) {
-	_, err = d.http.Request(methods.GetBalance, params, &balance)
+	_, err = d.Request(methods.GetBalance, params, &balance)
 	return
 }
 
 func (d *RPC) HasBalance(params GetBalanceParams) (hasBalance bool, err error) {
 	var result map[string]bool
-	_, err = d.http.Request(methods.HasBalance, params, &result)
+	_, err = d.Request(methods.HasBalance, params, &result)
 	hasBalance = result["exists"]
 	return
 }
 
 func (d *RPC) GetBalanceAtTopoheight(params GetBalanceAtTopoheightParams) (balance VersionedBalance, err error) {
-	_, err = d.http.Request(methods.GetBalanceAtTopoheight, params, &balance)
+	_, err = d.Request(methods.GetBalanceAtTopoheight, params, &balance)
 	return
 }
 
 func (d *RPC) GetAsset(assetId string) (asset AssetData, err error) {
 	params := map[string]string{"asset": assetId}
-	_, err = d.http.Request(methods.GetAsset, params, &asset)
+	_, err = d.Request(methods.GetAsset, params, &asset)
 	return
 }
 
 func (d *RPC) GetAssets(params GetAssetsParams) (assets []AssetData, err error) {
-	_, err = d.http.Request(methods.GetAssets, params, &assets)
+	_, err = d.Request(methods.GetAssets, params, &assets)
 	return
 }
 
 func (d *RPC) CountAssets() (count uint64, err error) {
-	_, err = d.http.Request(methods.CountAssets, nil, &count)
+	_, err = d.Request(methods.CountAssets, nil, &count)
 	return
 }
 
 func (d *RPC) CountTransactions() (count uint64, err error) {
-	_, err = d.http.Request(methods.CountTransactions, nil, &count)
+	_, err = d.Request(methods.CountTransactions, nil, &count)
 	return
 }
 
 func (d *RPC) CountAccounts() (count uint64, err error) {
-	_, err = d.http.Request(methods.CountAccounts, nil, &count)
+	_, err = d.Request(methods.CountAccounts, nil, &count)
 	return
 }
 
 func (d *RPC) GetTips() (tips []string, err error) {
-	_, err = d.http.Request(methods.GetTips, nil, &tips)
+	_, err = d.Request(methods.GetTips, nil, &tips)
 	return
 }
 
 func (d *RPC) P2PStatus() (status P2PStatusResult, err error) {
-	_, err = d.http.Request(methods.P2PStatus, nil, &status)
+	_, err = d.Request(methods.P2PStatus, nil, &status)
 	return
 }
 
 func (d *RPC) GetDAGOrder(params GetTopoheightRangeParams) (hashes []string, err error) {
-	_, err = d.http.Request(methods.GetDAGOrder, params, &hashes)
+	_, err = d.Request(methods.GetDAGOrder, params, &hashes)
 	return
 }
 
 func (d *RPC) SubmitBlock(params SubmitBlockParams) (result bool, err error) {
-	_, err = d.http.Request(methods.SubmitBlock, params, &result)
+	_, err = d.Request(methods.SubmitBlock, params, &result)
 	return
 }
 
 func (d *RPC) SubmitTransaction(data string) (result bool, err error) {
 	params := map[string]string{"data": data}
-	_, err = d.http.Request(methods.SubmitTransaction, params, &result)
+	_, err = d.Request(methods.SubmitTransaction, params, &result)
 	return
 }
 
 func (d *RPC) GetMempool() (txs []TransactionResponse, err error) {
-	_, err = d.http.Request(methods.GetMempool, nil, &txs)
+	_, err = d.Request(methods.GetMempool, nil, &txs)
 	return
 }
 
 func (d *RPC) GetMempoolCache(params GetMempoolCacheParams) (result GetMempoolCacheResult, err error) {
-	_, err = d.http.Request(methods.GetMempoolCache, params, &result)
+	_, err = d.Request(methods.GetMempoolCache, params, &result)
 	return
 }
 
 func (d *RPC) GetTransaction(hash string) (tx TransactionResponse, err error) {
 	params := map[string]string{"hash": hash}
-	_, err = d.http.Request(methods.GetTransaction, params, &tx)
+	_, err = d.Request(methods.GetTransaction, params, &tx)
 	return
 }
 
 func (d *RPC) GetTransactions(params GetTransactionsParams) (txs []TransactionResponse, err error) {
-	_, err = d.http.Request(methods.GetTransactions, params, &txs)
+	_, err = d.Request(methods.GetTransactions, params, &txs)
 	return
 }
 
 func (d *RPC) GetBlocksRangeByTopoheight(params GetTopoheightRangeParams) (blocks []Block, err error) {
-	_, err = d.http.Request(methods.GetBlocksRangeByTopoheight, params, &blocks)
+	_, err = d.Request(methods.GetBlocksRangeByTopoheight, params, &blocks)
 	return
 }
 
 func (d *RPC) GetBlocksRangeByHeight(params GetHeightRangeParams) (blocks []Block, err error) {
-	_, err = d.http.Request(methods.GetBlocksRangeByHeight, params, &blocks)
+	_, err = d.Request(methods.GetBlocksRangeByHeight, params, &blocks)
 	return
 }
 
 func (d *RPC) GetAccounts(params GetAccountsParams) (addresses []string, err error) {
-	_, err = d.http.Request(methods.GetAccounts, params, &addresses)
+	_, err = d.Request(methods.GetAccounts, params, &addresses)
 	return
 }
 
 func (d *RPC) GetAccountHistory(addr string) (history []AccountHistory, err error) {
 	params := map[string]string{"address": addr}
-	_, err = d.http.Request(methods.GetAccountHistory, params, &history)
+	_, err = d.Request(methods.GetAccountHistory, params, &history)
 	return
 }
 
 func (d *RPC) GetAccountAssets(addr string) (assets []string, err error) {
 	params := map[string]string{"address": addr}
-	_, err = d.http.Request(methods.GetAccountAssets, params, &assets)
+	_, err = d.Request(methods.GetAccountAssets, params, &assets)
 	return
 }
 
 func (d *RPC) GetPeers() (result GetPeersResult, err error) {
-	_, err = d.http.Request(methods.GetPeers, nil, &result)
+	_, err = d.Request(methods.GetPeers, nil, &result)
 	return
 }
 
 func (d *RPC) GetDevFeeThresholds() (fees []Fee, err error) {
-	_, err = d.http.Request(methods.GetDevFeeThresholds, nil, &fees)
+	_, err = d.Request(methods.GetDevFeeThresholds, nil, &fees)
 	return
 }
 
 func (d *RPC) GetSizeOnDisk() (sizeOnDisk SizeOnDisk, err error) {
-	_, err = d.http.Request(methods.GetSizeOnDisk, nil, &sizeOnDisk)
+	_, err = d.Request(methods.GetSizeOnDisk, nil, &sizeOnDisk)
 	return
 }
 
 func (d *RPC) IsTxExecutedInBlock(params IsTxExecutedInBlockParams) (executed bool, err error) {
-	_, err = d.http.Request(methods.IsTxExecutedInBlock, params, &executed)
+	_, err = d.Request(methods.IsTxExecutedInBlock, params, &executed)
 	return
 }
 
 func (d *RPC) GetAccountRegistrationTopoheight(addr string) (topoheight uint64, err error) {
 	params := map[string]string{"address": addr}
-	_, err = d.http.Request(methods.GetAccountRegistrationTopoheight, params, &topoheight)
+	_, err = d.Request(methods.GetAccountRegistrationTopoheight, params, &topoheight)
 	return
 }
 
 func (d *RPC) IsAccountRegistered(params IsAccountRegisteredParams) (exists bool, err error) {
-	_, err = d.http.Request(methods.IsAccountRegistered, params, &exists)
+	_, err = d.Request(methods.IsAccountRegistered, params, &exists)
 	return
 }
 
 func (d *RPC) GetDifficulty() (result GetDifficultyResult, err error) {
-	_, err = d.http.Request(methods.GetDifficulty, nil, &result)
+	_, err = d.Request(methods.GetDifficulty, nil, &result)
 	return
 }
 
 func (d *RPC) ValidateAddress(params ValidateAddressParams) (result ValidateAddressResult, err error) {
-	_, err = d.http.Request(methods.ValidateAddress, params, &result)
+	_, err = d.Request(methods.ValidateAddress, params, &result)
 	return
 }
 
 func (d *RPC) ExtractKeyFromAddress(params ExtractKeyFromAddressParams) (key ExtractKeyFromAddressResult, err error) {
-	_, err = d.http.Request(methods.ExtractKeyFromAddress, params, &key)
+	_, err = d.Request(methods.ExtractKeyFromAddress, params, &key)
 	return
 }
 
 func (d *RPC) GetMinerWork(params GetMinerWorkParams) (result GetMinerWorkResult, err error) {
-	_, err = d.http.Request(methods.GetMinerWork, params, &result)
+	_, err = d.Request(methods.GetMinerWork, params, &result)
 	return
 }
 
 func (d *RPC) SplitAddress(params SplitAddressParams) (result SplitAddressResult, err error) {
-	_, err = d.http.Request(methods.SplitAddress, params, &result)
+	_, err = d.Request(methods.SplitAddress, params, &result)
 	return
 }
 
 func (d *RPC) GetHardForks() (result []HardFork, err error) {
-	_, err = d.http.Request(methods.GetHardForks, nil, &result)
+	_, err = d.Request(methods.GetHardForks, nil, &result)
 	return
 }
 
 func (d *RPC) GetEstimatedFeeRates() (result FeeRatesEstimated, err error) {
-	_, err = d.http.Request(methods.GetEstimatedFeeRates, nil, &result)
+	_, err = d.Request(methods.GetEstimatedFeeRates, nil, &result)
 	return
 }
 
 func (d *RPC) GetPrunedTopoheight() (result uint64, err error) {
-	_, err = d.http.Request(methods.GetPrunedTopoheight, nil, &result)
+	_, err = d.Request(methods.GetPrunedTopoheight, nil, &result)
 	return
 }
 
 func (d *RPC) GetTransactionExecutor(params GetTransactionExecutorParams) (result GetTransactionExecutorResult, err error) {
-	_, err = d.http.Request(methods.GetTransactionExecutor, params, &result)
+	_, err = d.Request(methods.GetTransactionExecutor, params, &result)
 	return
 }
 
 func (d *RPC) HasMultisigAtTopoheight(params HasMultisigAtTopoheightParams) (result bool, err error) {
-	_, err = d.http.Request(methods.HasMultisigAtTopoheight, params, &result)
+	_, err = d.Request(methods.HasMultisigAtTopoheight, params, &result)
 	return
 }
 
 func (d *RPC) GetMultisigAtTopoheight(params GetMultisigAtTopoheightParams) (result GetMultisigAtTopoheightResult, err error) {
-	_, err = d.http.Request(methods.GetMultisigAtTopoheight, params, &result)
+	_, err = d.Request(methods.GetMultisigAtTopoheight, params, &result)
 	return
 }
 
 func (d *RPC) GetMultisig(params GetMultisigParams) (result GetMultisigResult, err error) {
-	_, err = d.http.Request(methods.GetMultisig, params, &result)
+	_, err = d.Request(methods.GetMultisig, params, &result)
 	return
 }
 
 func (d *RPC) HasMultisig(params HasMultisigParams) (result bool, err error) {
-	_, err = d.http.Request(methods.HasMultisig, params, &result)
+	_, err = d.Request(methods.HasMultisig, params, &result)
 	return
 }
 
 func (d *RPC) GetContractOutputs(params GetContractOutputsParams) (result []ContractOutput, err error) {
 	var outputs []interface{}
-	_, err = d.http.Request(methods.GetContractOutputs, params, &outputs)
+	_, err = d.Request(methods.GetContractOutputs, params, &outputs)
 	result = parseContractOutputs(outputs)
 	return
 }
 
 func (d *RPC) GetContractModule(params GetContractModuleParams) (result GetContractModuleResult, err error) {
-	_, err = d.http.Request(methods.GetContractModule, params, &result)
+	_, err = d.Request(methods.GetContractModule, params, &result)
 	return
 }
 
 func (d *RPC) GetContractData(params GetContractDataParams) (result interface{}, err error) {
-	_, err = d.http.Request(methods.GetContractData, params, &result)
+	_, err = d.Request(methods.GetContractData, params, &result)
 	return
 }
 
 func (d *RPC) GetContractDataAtTopoheight(params GetContractDataAtTopoheightParams) (result interface{}, err error) {
-	_, err = d.http.Request(methods.GetContractDataAtTopoheight, params, &result)
+	_, err = d.Request(methods.GetContractDataAtTopoheight, params, &result)
 	return
 }
 
 func (d *RPC) GetContractBalance(params GetContractBalanceParams) (result GetContractBalanceResult, err error) {
-	_, err = d.http.Request(methods.GetContractBalance, params, &result)
+	_, err = d.Request(methods.GetContractBalance, params, &result)
 	return
 }
 
 func (d *RPC) GetContractBalanceAtTopoheight(params GetContractBalanceAtTopoheightParams) (result GetContractBalanceAtTopoheightResult, err error) {
-	_, err = d.http.Request(methods.GetContractBalanceAtTopoheight, params, &result)
+	_, err = d.Request(methods.GetContractBalanceAtTopoheight, params, &result)
 	return
 }
 
 func (d *RPC) CountContracts() (result uint64, err error) {
-	_, err = d.http.Request(methods.CountAccounts, nil, &result)
+	_, err = d.Request(methods.CountAccounts, nil, &result)
 	return
 }
 
 func (d *RPC) MakeIntegratedAddress(params MakeIntegratedAddressParams) (result string, err error) {
-	_, err = d.http.Request(methods.MakeIntegratedAddress, params, &result)
+	_, err = d.Request(methods.MakeIntegratedAddress, params, &result)
 	return
 }
 
 func (d *RPC) DecryptExtraData(params DecryptExtraDataParams) (result interface{}, err error) {
-	_, err = d.http.Request(methods.DecryptExtraData, params, &result)
+	_, err = d.Request(methods.DecryptExtraData, params, &result)
 	return
 }
 
--- wallet/rpc.go
+++ wallet/rpc.go
@@ -40,73 +40,81 @@ func NewRPC(url string, username string, password string) (*RPC, error) {
 	return daemon, nil
 }
 
+func (d *RPC) BatchRequest(requests []rpc.RPCRequest, result map[int64]interface{}) (res *http.Response, errs []error) {
+	return d.http.BatchRequest(requests, result)
+}
+
+func (d *RPC) Request(method string, params interface{}, result interface{}) (res *http.Response, err error) {
+	return d.http.Request(method, params, result)
+}
+
 func (d *RPC) GetVersion() (version string, err error) {
-	_, err = d.http.Request(methods.GetVersion, nil, &version)
+	_, err = d.Request(methods.GetVersion, nil, &version)
 	return
 }
 
 func (d *RPC) GetNetwork() (network string, err error) {
-	_, err = d.http.Request(methods.GetNetwork, nil, &network)
+	_, err = d.Request(methods.GetNetwork, nil, &network)
 	return
 }
 
 func (d *RPC) GetNonce() (nonce uint64, err error) {
-	_, err = d.http.Request(methods.GetNonce, nil, &nonce)
+	_, err = d.Request(methods.GetNonce, nil, &nonce)
 	return
 }
 
 func (d *RPC) GetTopoheight() (topoheight uint64, err error) {
-	_, err = d.http.Request(methods.GetTopoheight, nil, &topoheight)
+	_, err = d.Request(methods.GetTopoheight, nil, &topoheight)
 	return
 }
 
 func (d *RPC) GetAddress(params GetAddressParams) (address string, err error) {
-	_, err = d.http.Request(methods.GetAddress, params, &address)
+	_, err = d.Request(methods.GetAddress, params, &address)
 	return
 }
 
 func (d *RPC) SplitAddress(params SplitAddressParams) (result SplitAddressResult, err error) {
-	_, err = d.http.Request(methods.SplitAddress, params, &result)
+	_, err = d.Request(methods.SplitAddress, params, &result)
 	return
 }
 
 func (d *RPC) Rescan(params RescanParams) (success bool, err error) {
-	_, err = d.http.Request(methods.Rescan, params, &success)
+	_, err = d.Request(methods.Rescan, params, &success)
 	return
 }
 
 func (d *RPC) GetBalance(params GetBalanceParams) (balance uint64, err error) {
-	_, err = d.http.Request(methods.GetBalance, params, &balance)
+	_, err = d.Request(methods.GetBalance, params, &balance)
 	return
 }
 
 func (d *RPC) HasBalance(params GetBalanceParams) (exists bool, err error) {
-	_, err = d.http.Request(methods.HasBalance, params, &exists)
+	_, err = d.Request(methods.HasBalance, params, &exists)
 	return
 }
 
 func (d *RPC) GetTrackedAssets() (assets []string, err error) {
-	_, err = d.http.Request(methods.GetTrackedAssets, nil, &assets)
+	_, err = d.Request(methods.GetTrackedAssets, nil, &assets)
 	return
 }
 
 func (d *RPC) GetAssetPrecision(params GetAssetPrecisionParams) (decimals int, err error) {
-	_, err = d.http.Request(methods.GetAssetPrecision, params, &decimals)
+	_, err = d.Request(methods.GetAssetPrecision, params, &decimals)
 	return
 }
 
 func (d *RPC) GetAssets() (assets map[string]Asset, err error) {
-	_, err = d.http.Request(methods.GetAssets, nil, &assets)
+	_, err = d.Request(methods.GetAssets, nil, &assets)
 	return
 }
 
 func (d *RPC) GetAsset(params GetAssetParams) (asset Asset, err error) {
-	_, err = d.http.Request(methods.GetAsset, params, &asset)
+	_, err = d.Request(methods.GetAsset, params, &asset)
 	return
 }
 
 func (d *RPC) GetTransaction(params GetTransactionParams) (transaction TransactionEntry, err error) {
-	_, err = d.http.Request(methods.GetTransaction, params, &transaction)
+	_, err = d.Request(methods.GetTransaction, params, &transaction)
 	return
 }
 
@@ -115,123 +123,123 @@ func (d *RPC) BuildTransaction(params BuildTransactionParams) (result Transactio
 		return
 	}
 
-	_, err = d.http.Request(methods.BuildTransaction, params, &result)
+	_, err = d.Request(methods.BuildTransaction, params, &result)
 	return
 }
 
 func (d *RPC) BuildTransactionOffline(params BuildTransactionOfflineParams) (result TransactionResponse, err error) {
-	_, err = d.http.Request(methods.BuildTransactionOffline, params, &result)
+	_, err = d.Request(methods.BuildTransactionOffline, params, &result)
 	return
 }
 
 func (d *RPC) BuildUnsignedTransaction(params BuildUnsignedTransactionParams) (result UnsignedTransactionResponse, err error) {
-	_, err = d.http.Request(methods.BuildUnsignedTransaction, params, &result)
+	_, err = d.Request(methods.BuildUnsignedTransaction, params, &result)
 	return
 }
 
 func (d *RPC) SignUnsignedTransaction(params SignUnsignedTransactionParams) (result SignatureId, err error) {
-	_, err = d.http.Request(methods.SignUnsignedTransaction, params, &result)
+	_, err = d.Request(methods.SignUnsignedTransaction, params, &result)
 	return
 }
 
 func (d *RPC) FinalizeUnsignedTransaction(params FinalizeUnsignedTransactionParams) (result TransactionResponse, err error) {
-	_, err = d.http.Request(methods.FinalizeUnsignedTransaction, params, &result)
+	_, err = d.Request(methods.FinalizeUnsignedTransaction, params, &result)
 	return
 }
 
 func (d *RPC) ClearTxCache() (result bool, err error) {
-	_, err = d.http.Request(methods.ClearTxCache, nil, &result)
+	_, err = d.Request(methods.ClearTxCache, nil, &result)
 	return
 }
 
 func (d *RPC) ListTransactions(params ListTransactionsParams) (txs []TransactionEntry, err error) {
-	_, err = d.http.Request(methods.ListTransactions, params, &txs)
+	_, err = d.Request(methods.ListTransactions, params, &txs)
 	return
 }
 
 func (d *RPC) IsOnline() (online bool, err error) {
-	_, err = d.http.Request(methods.IsOnline, nil, &online)
+	_, err = d.Request(methods.IsOnline, nil, &online)
 	return
 }
 
 func (d *RPC) SetOnlineMode() (success bool, err error) {
-	_, err = d.http.Request(methods.SetOnlineMode, nil, &success)
+	_, err = d.Request(methods.SetOnlineMode, nil, &success)
 	return
 }
 
 func (d *RPC) SetOfflineMode() (success bool, err error) {
-	_, err = d.http.Request(methods.SetOfflineMode, nil, &success)
+	_, err = d.Request(methods.SetOfflineMode, nil, &success)
 	return
 }
 
 func (d *RPC) SignData(data data.Element) (signature string, err error) {
 	// note: SignData parse params null value to {}
-	_, err = d.http.Request(methods.SignData, data, &signature)
+	_, err = d.Request(methods.SignData, data, &signature)
 	return
 }
 
 func (d *RPC) EstimateFees(params EstimateFeesParams) (amount uint64, err error) {
-	_, err = d.http.Request(methods.EstimateFees, params, &amount)
+	_, err = d.Request(methods.EstimateFees, params, &amount)
 	return
 }
 
 func (d *RPC) EstimateExtraDataSize(params EstimateExtraDataSizeParams) (result EstimateExtraDataSizeResult, err error) {
-	_, err = d.http.Request(methods.EstimateExtraDataSize, params, &result)
+	_, err = d.Request(methods.EstimateExtraDataSize, params, &result)
 	return
 }
 
 func (d *RPC) NetworkInfo() (result NetworkInfoResult, err error) {
-	_, err = d.http.Request(methods.NetworkInfo, nil, &result)
+	_, err = d.Request(methods.NetworkInfo, nil, &result)
 	return
 }
 
 func (d *RPC) DecryptExtraData(params DecryptExtraDataParams) (result PlaintextExtraData, err error) {
-	_, err = d.http.Request(methods.DecryptExtraData, params, &result)
+	_, err = d.Request(methods.DecryptExtraData, params, &result)
 	return
 }
 
 func (d *RPC) DecryptCiphertext(params DecryptCiphertextParams) (result uint64, err error) {
-	_, err = d.http.Request(methods.DecryptCiphertext, params, &result)
+	_, err = d.Request(methods.DecryptCiphertext, params, &result)
 	return
 }
 
 func (d *RPC) GetMatchingKeys(params GetMatchingKeysParams) (result []interface{}, err error) {
-	_, err = d.http.Request(methods.GetMatchingKeys, params, &result)
+	_, err = d.Request(methods.GetMatchingKeys, params, &result)
 	return
 }
 
 func (d *RPC) CountMatchingEntries(params CountMatchingEntriesParams) (result uint64, err error) {
-	_, err = d.http.Request(methods.CountMatchingEntries, params, &result)
+	_, err = d.Request(methods.CountMatchingEntries, params, &result)
 	return
 }
 
 func (d *RPC) GetValueFromKey(params GetValueFromKeyParams) (result interface{}, err error) {
-	_, err = d.http.Request(methods.GetValueFromKey, params, &result)
+	_, err = d.Request(methods.GetValueFromKey, params, &result)
 	return
 }
 
 func (d *RPC) Store(params StoreParams) (result bool, err error) {
-	_, err = d.http.Request(methods.Store, params, &result)
+	_, err = d.Request(methods.Store, params, &result)
 	return
 }
 
 func (d *RPC) Delete(params StoreParams) (result bool, err error) {
-	_, err = d.http.Request(methods.Delete, params, &result)
+	_, err = d.Request(methods.Delete, params, &result)
 	return
 }
 
 func (d *RPC) DeleteTreeEntries(params DeleteTreeEntriesParams) (result bool, err error) {
-	_, err = d.http.Request(methods.DeleteTreeEntries, params, &result)
+	_, err = d.Request(methods.DeleteTreeEntries, params, &result)
 	return
 }
 
 func (d *RPC) HasKey(params HasKeyParams) (result bool, err error) {
-	_, err = d.http.Request(methods.HasKey, params, &result)
+	_, err = d.Request(methods.HasKey, params, &result)
 	return
 }
 
 func (d *RPC) QueryDB(params QueryDBParams) (result QueryResult, err error) {
-	_, err = d.http.Request(methods.QueryDB, params, &result)
+	_, err = d.Request(methods.QueryDB, params, &result)
 	return
 }
 
